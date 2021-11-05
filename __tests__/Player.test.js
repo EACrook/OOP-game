187a1b6708779512ea6a346ef761df39ed191458
Emily Crook <Emily.A.Crook96@gmail.com>
2021-11-05 08:05:58 -0500

    testing and functinoality added to return player stats and check player inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,3 @@
-// const { test, expect, jest } = require('@jest/globals');
-const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 const SelfCare = require('../lib/SelfCare');
 
@@ -11,10 +9,29 @@ test('creates a player object', () => {
     expect(player.name).toBe('Emily');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.skills).toEqual(expect.any(Number));
-    expect(player.skills).toEqual(expect.any(Number));
+    expect(player.relationships).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test('gets player stats as an object', () => {
+    const player = new Player('Emily');
+
+    expect(player.getStats()).toHaveProperty('selfcare');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('relationships');
+    expect(player.getStats()).toHaveProperty('skills');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Emily');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
 
 console.log(new SelfCare());
