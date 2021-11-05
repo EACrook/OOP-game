2cd25f9057bc947f23a05e735b55642f1ca13328
Emily Crook <Emily.A.Crook96@gmail.com>
2021-11-04 20:16:08 -0500

    initial set up for SelfCare constructor function and testing

--- __tests__/SelfCare.test.js
+++ __tests__/SelfCare.test.js
@@ -6,3 +6,11 @@ test('creates a self care object', () => {
     expect(selfCare.name).toBe('health');
     expect(selfCare.value).toEqual(expect.any(Number));
 });
+
+test('creates a random self care object', () => {
+    const selfCare = new SelfCare();
+
+    expect(selfCare.name).toEqual(expect.any(String));
+    expect(selfCare.name.length).toBeGreaterThan(0);
+    expect(selfCare.value).toEqual(expect.any(Number));
+});
